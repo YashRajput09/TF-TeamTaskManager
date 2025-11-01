cf67a4b9879e397b254050776fadb2ef5efa02d4
Yash Rajput <rajputyash8561@gmail.com>
2025-11-01 11:47:25 +0530

    setup backend

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Listening on port: ${port}`);
+});
+app.use("/",(req, res)=>{
+    res.send("Radhe Radhe");
+})
