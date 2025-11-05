f557950f4dcb1e3ad9d0dcb88b17db689f9c30be
Yash Rajput <rajputyash8561@gmail.com>
2025-11-05 21:59:33 +0530

    feat: get profile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {isAuthenticated} from '../middleware/authenticateUser.js'
-import {signUpUser, logInUser, logOutUser} from '../controller/user.controller.js';
+import {signUpUser, logInUser, logOutUser, getMyProfile} from '../controller/user.controller.js';
 
 const router = express.Router();
 
 router.route('/signup').post(signUpUser);
 router.route('/login').post(logInUser);
 router.route('/logout').post(isAuthenticated, logOutUser);
+router.route("/myprofile").get(isAuthenticated, getMyProfile);
+
 
 export default router;
